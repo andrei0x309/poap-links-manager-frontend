dd83e22642f67cfc54ce982b292d3b2af1a06fc2
Andrei O <andrei@flashsoft.eu>
2021-10-21 02:18:22 +0300

    fix typo

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -53,7 +53,7 @@
                 dark:border-gray-600 dark:bg-gray-700 dark:text-gray-200
               "
             >
-              Naigate
+              Navigate
             </button>
           </div>
           <div
